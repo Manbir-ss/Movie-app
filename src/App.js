3acee33b22b199d36016794553c76ad025d9d1f8
Manbir-ss <59577171+Manbir-ss@users.noreply.github.com>
2024-03-16 17:00:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { SearchIcon } from 'react-icons';
+import './App.css';
+import Main from './My componenets/anime_src';
+import './My componenets/anime_style.css';
+import Cards from './My componenets/animes_list.js'
+function App() {
+  return(
+    <Main/>
+  )
+}
+
+export default App;
